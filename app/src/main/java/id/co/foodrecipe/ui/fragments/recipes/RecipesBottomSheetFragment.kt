51274d1d8ebb38b601f398e8792b40688c6f9b05
Raihan <raihanarman8@gmail.com>
2021-03-22 10:01:38 +0800

    Chip

---
+++ app/src/main/java/id/co/foodrecipe/ui/fragments/recipes/RecipesBottomSheetFragment.kt
@@ -0,0 +1,19 @@
+package id.co.foodrecipe.ui.fragments.recipes
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import id.co.foodrecipe.R
+
+class RecipesBottomSheetFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recipes_bottom_sheet, container, false)
+    }
+
+}
